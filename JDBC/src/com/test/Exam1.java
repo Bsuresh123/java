5a9b716dbdf5d87bccb4ea04256bc679336c81d6
Suresh B <Suresh B@suresh>
2023-09-25 15:18:21 +0530

    jdbc

--- JDBC/src/com/test/Exam1.java
+++ JDBC/src/com/test/Exam1.java
@@ -16,14 +16,15 @@ public class Exam1 {
 		String sql2="update suri set sname='dhqd' where sno=1";
 //		String sql3="delete from  suri where sno=1";
 		String sql4="select * from suri";
+		
 		st.execute(sql1);
 		ResultSet rs=st.executeQuery(sql4);
 		while(rs.next())
 		{
 			System.out.println(rs.getInt(1)+rs.getString(2)+rs.getString(3));
 		}
+		con.close();
 		System.out.println("hello");  
-
 
 
 	
